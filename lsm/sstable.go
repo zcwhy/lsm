64033c5b4f97d4b9ebc760d3648e4ee739e4acc0
zcwhy <845562684@qq.com>
2025-08-26 20:28:22 +0800

    实现sstable读写流程

--- lsm/sstable.go
+++ lsm/sstable.go
@@ -1,7 +1,10 @@
 package lsm
 
 import (
-	"fmt"
+	"bufio"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
 	"io"
 	"os"
 )
@@ -15,19 +18,21 @@ type SSTable struct {
 }
 
 type Position struct {
-	Len int64
-	Off int64
+	Len uint64
+	Off uint64
 }
 
 // 16bytes
 type MetaInfo struct {
-	IndexBlockOffset int64
-	IndexBlockLen    int64
+	IndexBlockOffset uint64
+	IndexBlockLen    uint64
 }
 
 func LoadSStableFromFile(filePath string) *SSTable {
 	s := &SSTable{
-		FilePath: filePath,
+		FilePath:   filePath,
+		IndexBlock: make(map[string]*Position),
+		MetaBlock:  &MetaInfo{},
 	}
 
 	f, err := os.Open(filePath)
@@ -38,38 +43,121 @@ func LoadSStableFromFile(filePath string) *SSTable {
 	s.f = f
 
 	s.LoadMetaBlock()
-	s.LoadMetaBlock()
+	s.LoadIndexBlock()
 
 	return s
 }
 
-func (s *SSTable) LoadMetaBlock() {
+func WriteToSStable(datas []*Entry, filePath string) (*SSTable, error) {
+	f, err := os.OpenFile(filePath, os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	w := bufio.NewWriterSize(f, 4096)
+
+	s := &SSTable{
+		IndexBlock: make(map[string]*Position),
+		MetaBlock:  new(MetaInfo),
+	}
+
+	var off uint64
+	// write data block, build index info
+	for _, e := range datas {
+		bytes, err := e.Encode()
+		if err != nil {
+			return nil, err
+		}
+
+		n, err := w.Write(bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		s.IndexBlock[e.Key] = &Position{Off: off, Len: uint64(n)}
+		off += uint64(n)
+	}
+
+	// write index block
+	indexBytes, err := json.Marshal(s.IndexBlock)
+	if err != nil {
+		return nil, err
+	}
+	n, err := w.Write(indexBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	s.MetaBlock.IndexBlockOffset = off
+	s.MetaBlock.IndexBlockLen = uint64(n)
+
+	// write meta block
+	metaBytes := s.MetaBlock.Encode()
+	_, err = w.Write(metaBytes)
+	if err != nil {
+		return nil, err
+	}
+	return s, w.Flush()
+}
+
+func (s *SSTable) LoadMetaBlock() error {
 	info, err := s.f.Stat()
 	if err != nil {
-		panic(err)
+		return err
 	}
-	size := info.Size()
 
 	// 确保文件至少16字节
-	if size < 16 {
-		panic("file too small")
+	if info.Size() < 16 {
+		return errors.New("Invalid meta size")
 	}
 
 	// 从文件末尾向前移动16字节
 	_, err = s.f.Seek(-16, io.SeekEnd)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	// 读取16字节
 	buf := make([]byte, 16)
 	_, err = s.f.Read(buf)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	fmt.Printf("Last 16 bytes: % x\n", buf)
+	return s.MetaBlock.Decode(buf)
 }
 
-func (s *SSTable) LoadIndex() {
+func (s *SSTable) LoadIndexBlock() error {
+	idxOff := s.MetaBlock.IndexBlockOffset
+	idxBytes := make([]byte, s.MetaBlock.IndexBlockLen)
+
+	_, err := s.f.ReadAt(idxBytes, int64(idxOff))
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(idxBytes, &s.IndexBlock); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MetaInfo) Encode() []byte {
+	metaBytes := make([]byte, 16)
+
+	binary.BigEndian.PutUint64(metaBytes[0:8], uint64(m.IndexBlockLen))
+	binary.BigEndian.PutUint64(metaBytes[8:16], uint64(m.IndexBlockOffset))
+
+	return metaBytes
+}
+
+func (m *MetaInfo) Decode(metaBytes []byte) error {
+	if len(metaBytes) != 16 {
+		return errors.New("Invalid metainfo length")
+	}
+
+	m.IndexBlockLen = binary.BigEndian.Uint64(metaBytes[0:8])
+	m.IndexBlockOffset = binary.BigEndian.Uint64(metaBytes[8:16])
+
+	return nil
 }
---
+++ lsm/sstable_test.go
@@ -0,0 +1,34 @@
+package lsm
+
+import (
+	"os"
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+	entries := []*Entry{
+		{Key: "a", Val: []byte("aaaa"), Deleted: false},
+		{Key: "b", Val: []byte("bbbb"), Deleted: false},
+	}
+
+	t.Log(WriteToSStable(entries, "./sstable"))
+}
+
+func TestLoadMeta(t *testing.T) {
+	s := &SSTable{MetaBlock: &MetaInfo{}}
+	s.f, _ = os.Open("./sstable")
+	s.LoadMetaBlock()
+	t.Log(s.MetaBlock)
+}
+
+func TestRead(t *testing.T) {
+	s := LoadSStableFromFile("./sstable")
+
+	t.Log(s.IndexBlock)
+}
+
+func TestFile(t *testing.T) {
+	f, _ := os.Open("./sstable")
+	info, _ := f.Stat()
+	t.Log(info.Size())
+}
